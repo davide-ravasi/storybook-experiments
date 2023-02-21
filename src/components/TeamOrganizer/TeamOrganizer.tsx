a9faf5f3b36c1223b5ff95fd10e0d28cd4e83402
davide <info@davideravasi.com>
2023-02-21 12:41:29 +0000

    feat: added function to select member in team in temaorganizer

--- src/components/TeamOrganizer/TeamOrganizer.tsx
+++ src/components/TeamOrganizer/TeamOrganizer.tsx
@@ -7,19 +7,43 @@ interface ITeamOrganizer {
 }
 
 const TeamOrganizer = ({ players }: ITeamOrganizer) => {
-  const [team, setTeam] = useState("");
-  const [player, setPlayer] = useState("");
+  const [team, setTeam] = useState("team1");
+  const [playersList, setPlayersList] = useState(players);
+  const [team1, setTeam1] = useState([]);
+  const [team2, setTeam2] = useState([]);
+
+  // set team
+  // resset the teams
+  // only five in every team max
+  // random?
+  // 
+
+  const setPlayerOnTeam = (player) => {
+    if (team) {
+      const newPlayersList = playersList.filter(playerList => playerList !== player);
+      setPlayersList(newPlayersList);
+
+      if (team === 'team1') {
+        const newTeam1 = [...team1, player];
+        setTeam1(newTeam1);
+      }
+
+      const newTeam2 = [...team2, player];
+      setTeam2(newTeam2);
+    }
+  }
 
   return (
     <div>
-      <PlayerList players={players} setPlayer={setPlayer} />
+      <PlayerList players={playersList} setPlayer={setPlayerOnTeam} />
+      <TeamList title="Team 1" team={team1} />
+      <TeamList title="Team 2" team={team2} />
     </div>
   )
 }
 
 const PlayerList = ({ players, setPlayer }) => {
   const selectPlayer = (player) => {
-    console.log(player);
     setPlayer(player)
   };
   return (
@@ -31,6 +55,19 @@ const PlayerList = ({ players, setPlayer }) => {
       })}
     </form>
   )
+}
+
+const TeamList = ({ title, team }) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {team && team.map((player) => {
+          return <li>{player}</li>
+        })}
+      </ul>
+    </>
+  );
 }
 
 
